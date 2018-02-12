78f66fdff0db5faade6b6a96be181b118d31adf2
Tim Jordan <raeyejordan@yahoo.com>
2018-02-12 20:36:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeli=[];
+function takeANumber(array,name){
+  array.push(name);
+  return `Welcome, ${name}. You are number ${array.length} in line.`;
+}
+function nowServing(array){
+  let i=0;
+  if (i<array.length){
+    return `Currently serving ${array.shift(array)}.`;
+  }else{
+    return `There is nobody waiting to be served!`;
+  }
+}
+function currentLine(array){
+  let i=0;
+  if (i===array.length){
+    return `The line is currently empty.`;
+  }
+}
